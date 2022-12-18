2e944e9dba53d331ce3b26e83fde73164e298a84
Sleroq <cantundo@pm.me>
2022-12-18 22:34:25 +0300

    Created install script

---
+++ scripts/install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+cd "$(dirname "$SCRIPTPATH")"
+
+BIN="$(go env GOBIN)"
+
+if [ -z "$BIN" ]
+then
+    BIN="$(go env GOPATH)"/bin
+fi
+
+if [ -z "$BIN" ]
+then
+    echo "GOBIN and GOPATH are not set.
+Add binary (built/xi) to your \$PATH manually."
+fi
+
+scripts/build.sh
+
+cp built/xi "$BIN"/xi
